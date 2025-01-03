c8aed96cddfb8f7ce4effcf7b05eb62eb2fec3cd
MattUreal <matt221219@gmail.com>
2025-01-04 00:02:29 +0300

    navbar fix added new features

--- app/offerings/page.js
+++ app/offerings/page.js
@@ -9,6 +9,12 @@ const Offerings = () => {
         { id: 1, title: 'Software Engineer', location: 'Remote', description: 'Develop and maintain web applications.' },
         { id: 2, title: 'Product Manager', location: 'New York, NY', description: 'Lead product development and strategy.' },
         { id: 3, title: 'HR Specialist', location: 'San Francisco, CA', description: 'Manage recruitment and employee relations.' },
+        { id: 4, title: 'Software Engineer', location: 'Remote', description: 'Develop and maintain web applications.' },
+        { id: 5, title: 'Software Engineer', location: 'Remote', description: 'Develop and maintain web applications.' },
+        { id: 6, title: 'Product Manager', location: 'New York, NY', description: 'Lead product development and strategy.' },
+        { id: 7, title: 'HR Specialist', location: 'San Francisco, CA', description: 'Manage recruitment and employee relations.' },
+        { id: 8, title: 'Software Engineer', location: 'Remote', description: 'Develop and maintain web applications.' },
+
       ];
   return (
     <div >
@@ -16,9 +22,9 @@ const Offerings = () => {
       <Navbar />
       <div className="p-8 text-center">
          {/* List of Jobs */}
-          <section className="mt-8">
+          <section className="mt-4">
           <h2 className="text-2xl font-semibold mb-4">Available Jobs</h2>
-          <ul className="space-y-4">
+          <ul className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-16">
             {jobs.map((job) => (
               <li key={job.id} className="border p-4 rounded shadow">
                 <h3 className="text-xl font-bold">{job.title}</h3>
